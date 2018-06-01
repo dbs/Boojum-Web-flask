f75dc4e1122eaaff9763f7d6904fc7e4b2f3fe7e
Patrick Trottier <ptrottier@laurentian.ca>
2018-06-01 15:36:23 -0400

    Add search bar, make map scalable, tweak results Signed-off-by: Patrick Trottier <ptrottier@laurentian.ca>

--- webapp/static/map.js
+++ webapp/static/map.js
@@ -1,14 +1,26 @@
-new ol.Map({
-	target: 'map',
+var attribution = new ol.control.Attribution({
+        collapsible: false
+});
+
+var map = new ol.Map({
+	controls: ol.control.defaults({attribution: false}).extend([attribution]),
 	layers: [
 		new ol.layer.Tile({
 			source: new ol.source.OSM()
 		})
 	],
+	target: 'map',
 	view: new ol.View({
 		center: [0, 0],
 		zoom: 2
 	}),
-	// Hide controls with []
-	controls: []
 });
+
+function checkSize() {
+	var small = map.getSize()[0] < 600;
+	attribution.setCollasible(small);
+	attribution.setCollapsed(small);
+}
+
+window.addEventListener("resize", checkSize);
+checkSize();
